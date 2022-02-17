4b71e220013584ed3c15f80a5824aeb1dca6b078
carloodq <dallaquerciacarlo@gmail.com>
2022-02-17 13:46:18 +0800

    adding logic after to proceed

--- actions/actions.py
+++ actions/actions.py
@@ -29,6 +29,54 @@ class Validate_already_have_agent(FormValidationAction):
         return slots_mapped_in_domain
 
 
+        
+class action_rule_only_once(Action):
+
+    def name(self) -> Text:
+        return "action_rule_only_once"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        a = tracker.events
+
+        only_actions = []
+        only_intents = []
+
+        for x in a:
+            if x['event'] == 'user':
+                only_intents.append(x['parse_data']['intent']['name'])
+            elif x['event'] == 'bot':
+                try:
+                    only_actions.append(x['metadata']['utter_action'])
+                except:
+                    pass
+
+        
+        if only_actions[-1] == 'utter_greeting_sharon_prudential':
+            reply = 'utter_greeting_repeat_sharon_prudential'
+        
+        else:
+            reply = ''
+            for intent in [ ]:
+                if only_intents.count(intent) > 1:
+                    reply = 'utter_goodbye'
+
+            
+            if reply != 'utter_goodbye':
+                rules = {'question_related_to_age_info':'utter_age_question',
+                        'not_singaporean':'utter_goodbye',
+                        'question_speak_chinese':'utter_chinese_audio',
+                        'question_related_to_fees':'utter_cost_info',
+                        'question_related_to_scam':'utter_scam_reply',
+                        'question_related_to_manager':'utter_manager_introduction'}
+                reply = rules[only_intents[-1]]
+
+        dispatcher.utter_message(response = reply)
+        return []
+
+
 
 class action_fast_sharon(Action):
 
@@ -63,7 +111,7 @@ class action_fast_sharon(Action):
                 "'who_is', ", "'nlu_fallback', ", "'repeat', ",  "'question_why_cpf_and_prudential', ",
                 "'looking_for', ",
                 "'question_related_to_singpass_checking', ",
-                "'question_related_to_fees', ", "'question_related_to_age_info', ",
+                "'question_related_to_fees', ", "'question_related_to_age_info', ", 
                 "'question_related_to_scam', ",
                 "'question_related_to_distribution', ",
                 "'question_related_to_disclosure_details', ",
@@ -86,6 +134,7 @@ class action_fast_sharon(Action):
         two_no = ['reject','already_done']
         five_yes = ['confirm', 'morning','afternoon','evening', 'date_day']
         two_yes = ['confirm', 'age']
+        after_greeting = ['morning', 'afternoon', 'evening', 'age', 'date_day', 'confirm', 'reject', 'already_done']
         reply = 'utter_goodbye'
         if len_a < 4 and only_intents[-1] in ['afternoon', 'morning', 'evening', 'date_day']:
             reply = 'utter_busy_call_back'
@@ -97,12 +146,10 @@ class action_fast_sharon(Action):
         if len_a == 1:
             reply = 'utter_goodbye'
         elif len_a == 2:
-            if only_intents == ['start_conversation', 'confirm']:
-                reply = 'utter_discuss_nomination'
-            elif only_intents == ['start_conversation', 'reject']:
+            if only_intents in [['start_conversation', x] for x in after_greeting]:
                 reply = 'utter_discuss_nomination'
         elif len_a == 3:
-            if only_intents in [['start_conversation']+[x]+[xx] for x in ['reject','already_done', 'confirm'] for xx in ['reject','already_done', 'confirm'] ]:
+            if only_intents in [['start_conversation']+[x]+[xx] for x in after_greeting for xx in ['reject','already_done', 'confirm'] ]:
                 reply = 'utter_purpose'
             # elif only_intents == ['start_conversation', 'confirm', 'reject']:
             #     reply = 'utter_convincing'
@@ -114,33 +161,33 @@ class action_fast_sharon(Action):
             # contains any of the rejections
             # if only_intents in [['start_conversation']+[x]+[x]+['confirm'] for x in ['reject','already_done']]:
             #     reply = 'utter_appointment_call_time'
-            if only_intents in [['start_conversation']+[x]+[xx]+[t] for x in ['reject','already_done', 'confirm'] for xx in ['reject','already_done', 'confirm'] for t in ['afternoon', 'morning', 'evening', 'date_day', 'confirm']]:
+            if only_intents in [['start_conversation']+[x]+[xx]+[t] for x in after_greeting for xx in ['reject','already_done', 'confirm'] for t in ['afternoon', 'morning', 'evening', 'date_day', 'confirm']]:
                 reply = 'utter_appointment_call_time'
-            elif only_intents in [['start_conversation']+[x]+[xx]+['reject'] for x in ['reject','already_done', 'confirm'] for xx in ['reject','already_done', 'confirm']]:
+            elif only_intents in [['start_conversation']+[x]+[xx]+['reject'] for x in after_greeting for xx in ['reject','already_done', 'confirm']]:
                 reply = 'utter_convincing'
-            elif only_intents in [['start_conversation']+[x]+[xx]+['already_done'] for x in ['reject','already_done', 'confirm'] for xx in ['reject','already_done', 'confirm']]:
+            elif only_intents in [['start_conversation']+[x]+[xx]+['already_done'] for x in after_greeting for xx in ['reject','already_done', 'confirm']]:
                 reply = 'utter_done_nomination'
 
         elif len_a == 5:
             if only_intents in [['start_conversation']+[x]+[xx]+[i]+[t] for i in ['reject','already_done']\
-            for x in ['reject','already_done', 'confirm'] for xx in ['reject','already_done', 'confirm'] for t in ['afternoon', 'morning', 'evening', 'date_day', 'confirm']]:
+            for x in after_greeting for xx in ['reject','already_done', 'confirm'] for t in ['afternoon', 'morning', 'evening', 'date_day', 'confirm']]:
                 reply = 'utter_appointment_call_time'
-            elif only_intents[:-1] in [['start_conversation']+[x]+[xx]+[t] for x in ['reject','already_done', 'confirm'] for xx in ['reject','already_done', 'confirm'] for t in ['afternoon', 'morning', 'evening', 'date_day', 'confirm'] ]\
+            elif only_intents[:-1] in [['start_conversation']+[x]+[xx]+[t] for x in after_greeting for xx in ['reject','already_done', 'confirm'] for t in ['afternoon', 'morning', 'evening', 'date_day', 'confirm'] ]\
                 and only_intents[-1] in ['confirm','morning','afternoon','evening', 'date_day']:
                 reply = 'utter_age_info'
             # elif only_intents in [['start_conversation']+[x]+[xx]+['confirm'] for x in ['reject','already_done', 'confirm'] for xx in ['reject','already_done', 'confirm']]:
             #     reply = 'utter_appointment_call_time'
         elif len_a == 6:
-            if only_intents[:-1] in [['start_conversation']+[x]+[xx]+[i]+[t] for i in ['reject','already_done'] for x in ['reject','already_done', 'confirm'] for xx in ['reject','already_done', 'confirm'] for t in ['afternoon', 'morning', 'evening', 'date_day', 'confirm'] ]\
+            if only_intents[:-1] in [['start_conversation']+[x]+[xx]+[i]+[t] for i in ['reject','already_done'] for x in after_greeting for xx in ['reject','already_done', 'confirm'] for t in ['afternoon', 'morning', 'evening', 'date_day', 'confirm'] ]\
                 and only_intents[-1] in ['confirm','morning','afternoon','evening', 'date_day']:
                 reply = 'utter_age_info'
             # elif only_intents in [['start_conversation', 'confirm', 'confirm']+[x]+['confirm']+ [i] for i in ['confirm', 'morning','afternoon','evening', 'date_day']\
             #      for x in ['reject','already_done']]:
             #     reply = 'utter_age_info'
-            elif only_intents in [['start_conversation']+[x]+[xx]+[t]+[ii]+ [iii] for x in yes_no for xx in yes_no for ii in five_yes for iii in two_yes for t in ['afternoon', 'morning', 'evening', 'date_day', 'confirm']]:
+            elif only_intents in [['start_conversation']+[x]+[xx]+[t]+[ii]+ [iii] for x in after_greeting for xx in yes_no for ii in five_yes for iii in two_yes for t in ['afternoon', 'morning', 'evening', 'date_day', 'confirm']]:
                 reply = 'utter_acknowledgement_age'
         elif len_a == 7:
-            if only_intents in [['start_conversation']+[x]+[xx]+[ii]+[t]+ [iii] + [iiii] for x in yes_no for xx in yes_no for ii in two_no for iii in five_yes for iiii in two_yes for t in ['afternoon', 'morning', 'evening', 'date_day', 'confirm']]:
+            if only_intents in [['start_conversation']+[x]+[xx]+[ii]+[t]+ [iii] + [iiii] for x in after_greeting for xx in yes_no for ii in two_no for iii in five_yes for iiii in two_yes for t in ['afternoon', 'morning', 'evening', 'date_day', 'confirm']]:
                 reply = 'utter_acknowledgement_age'
               
 
